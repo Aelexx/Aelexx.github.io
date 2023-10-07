437c413cdd62cad5b54604a644c5f080ac5d051d
Aelexx <140091101+Aelexx@users.noreply.github.com>
2023-10-07 20:11:07 +0300

    Choose

--- JavaScript.js
+++ JavaScript.js
@@ -315,8 +315,8 @@ else {alert('Wrong...');};
 }
 
 function choose(){
-    const chooser = document.getElementById('chb5');
-    if(chooser){
+    //const chooser = document.getElementById('chb5');
+    if(document.getElementById('chb5').checked){
         window.location.href="https://analytics.google.com/analytics/web/?utm_source=marketingplatform.google.com&utm_medium=et&utm_campaign=marketingplatform.google.com%2Fabout%2Fanalytics%2F#/p409874903/realtime/overview?params=_u..nav%3Dmaui&collectionId=life-cycle";
 
     }
