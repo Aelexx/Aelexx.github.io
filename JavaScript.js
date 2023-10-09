9121698b84a6dd95c49f6cb6403aec8df0111736
Aelexx <140091101+Aelexx@users.noreply.github.com>
2023-10-09 17:33:58 +0300

    Ops

--- JavaScript.js
+++ JavaScript.js
@@ -314,22 +314,13 @@ else {alert('No authorization. Try another one');};
 }
 
 function redirect(){
-    switch (checked) {
-        case document.getElementById('chb1'):
-            window.open('https://analytics.google.com/analytics/web/?utm_source=marketingplatform.google.com&utm_medium=et&utm_campaign=marketingplatform.google.com%2Fabout%2Fanalytics%2F#/p409874903/reports/dashboard/edit?params=_u..nav%3Dmaui&r=lifecycle-acquisition-overview&ruid=lifecycle-acquisition-overview,life-cycle,acquisition&collectionId=life-cycle');
-          
-        case document.getElementById('chb5'):
-            window.open('https://analytics.google.com/analytics/web/?utm_source=marketingplatform.google.com&utm_medium=et&utm_campaign=marketingplatform.google.com%2Fabout%2Fanalytics%2F#/p409874903/realtime/overview?params=_u..nav%3Dmaui&collectionId=life-cycle');    
-        default:
-            break;
-    }
-    /*
+
     if(document.getElementById('chb1').checked == true){
         window.open('https://analytics.google.com/analytics/web/?utm_source=marketingplatform.google.com&utm_medium=et&utm_campaign=marketingplatform.google.com%2Fabout%2Fanalytics%2F#/p409874903/reports/dashboard/edit?params=_u..nav%3Dmaui&r=lifecycle-acquisition-overview&ruid=lifecycle-acquisition-overview,life-cycle,acquisition&collectionId=life-cycle');
     }  
     else if (document.getElementById('chb5').checked == true){
         window.open('https://analytics.google.com/analytics/web/?utm_source=marketingplatform.google.com&utm_medium=et&utm_campaign=marketingplatform.google.com%2Fabout%2Fanalytics%2F#/p409874903/realtime/overview?params=_u..nav%3Dmaui&collectionId=life-cycle');
-    } */
+    } 
 
 };
     
