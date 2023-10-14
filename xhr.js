2c5b9705f8c352018860db9aa0112d10ec2962b9
Aelexx <140091101+Aelexx@users.noreply.github.com>
2023-10-14 11:27:57 +0300

    xhr_response

--- xhr.js
+++ xhr.js
@@ -1,4 +1,7 @@
 const requestURL = 'https://jsonplaceholder.typicode.com/users';
 const xhr = new XMLHttpRequest();
 xhr.open('GET', requestURL);
+xhr.onload = () =>{
+    console.log(xhr.response);
+}
 xhr.send();
