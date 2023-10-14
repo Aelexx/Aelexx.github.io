bfe6d47da36e14d0556eb54297549ea82404ab4e
Aelexx <140091101+Aelexx@users.noreply.github.com>
2023-10-14 13:38:27 +0300

    onerrore

--- xhr.js
+++ xhr.js
@@ -2,13 +2,15 @@ const requestURL = 'https://jsonplaceholder.typicode.com/users';
 const xhr = new XMLHttpRequest();
 xhr.open('GET', requestURL);
 xhr.responseType = 'json';
-xhr.onload = () =>{
+xhr.onload = () => {
     if(xhr.status >= 400){
         console.error(xhr.response);
     }
     else{
         console.log(xhr.response);
-    }
-    
+    }  
+}
+xhr.onerror = () => {
+    console.log(xhr.response);
 }
 xhr.send();
