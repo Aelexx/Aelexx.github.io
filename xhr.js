84b42c6c659243dc031088a7a4ea35d3bdd62a19
Aelexx <140091101+Aelexx@users.noreply.github.com>
2023-10-14 14:10:31 +0300

    constupperbdy

--- xhr.js
+++ xhr.js
@@ -1,6 +1,7 @@
+const requestURL = 'https://jsonplaceholder.typicode.com/users';
+
 function sendRequest(method, url){
     return new Promise((resolve, reject) => {
-const requestURL = 'https://jsonplaceholder.typicode.com/users';
 const xhr = new XMLHttpRequest();
 xhr.open(method, url);
 xhr.responseType = 'json';
