6a7478a1e467b17a6d5d3c66765bdf68c9313e70
Aelexx <140091101+Aelexx@users.noreply.github.com>
2023-10-15 13:54:49 +0300

    parsee

--- Fetch.js
+++ Fetch.js
@@ -10,7 +10,15 @@ function sendRequest(method, url, body = null){
         headers: headers
     }
         ).then(response => {
-        return response.json()
+            if(response.ok){
+            return response.json()
+            }
+            return response.json().then(error => {
+                const er = new Error('Something wrong')
+                er.data = error
+                throw er
+            })
+        
     })
 }
 
