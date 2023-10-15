18774b343e5463e822095aa4ec1082e48829c559
Aelexx <140091101+Aelexx@users.noreply.github.com>
2023-10-15 11:17:17 +0300

    stringify

--- xhr.js
+++ xhr.js
@@ -5,8 +5,8 @@ function sendRequest(method, url, body = null){
 const xhr = new XMLHttpRequest()
 
 xhr.open(method, url)
-
 xhr.responseType = 'json'
+xhr.setRequestHeader('Content-Type', 'application/json')
 
 xhr.onload = () => {
     if(xhr.status >= 400){
