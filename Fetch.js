be2186cd482ce0c491faf0f1b43b3a14ecacd357
Aelexx <140091101+Aelexx@users.noreply.github.com>
2023-10-15 12:37:40 +0300

    dfsdfwe

--- Fetch.js
+++ Fetch.js
@@ -1,20 +1,28 @@
 const requestURL = 'https://jsonplaceholder.typicode.com/users'
 
 function sendRequest(method, url, body = null){
-    return fetch(url).then(response => {
+    const headers = {
+        'Content-Type': 'application/json'
+    }
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(body),
+        headers: headers
+    }
+        ).then(response => {
         return response.json()
     })
 }
 
-sendRequest('GET', requestURL)
-.then(data => console.log(data))
-.catch(err => console.log(err))
+// sendRequest('GET', requestURL)
+// .then(data => console.log(data))
+// .catch(err => console.log(err))
 
 const body = {
     name:'Alex',
     age: 24
 }
 
-// sendRequest('POST', requestURL, body)
-// .then(data => console.log(data))
-// .catch(err => console.log(err))
+sendRequest('POST', requestURL, body)
+.then(data => console.log(data))
+.catch(err => console.log(err))
