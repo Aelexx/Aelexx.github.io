3ef03a5b16c42b22c2bf672f377c35bafd230cca
Aelexx <140091101+Aelexx@users.noreply.github.com>
2023-10-16 17:14:37 +0300

    divs

--- plugins/modal.js
+++ plugins/modal.js
@@ -1,3 +1,30 @@
+function _createModal (){
+    const myModal = documentCreateElement('div');
+    modal.classList.add('amodal');
+    modal.insertAdjustHTML('afterbegin', `   
+
+    <div class="modal-overlay">
+        <div class="modal-window">
+
+            <div class="modal-header">
+            <span class="modal-title">Title</span>    
+            <span class="modal-close">&times;</span>
+            </div>
+
+            <div class="modal-body">
+            <p> See me good </p>
+            </div>
+
+            <div class="modal-footer">
+            <button>Okay</button>
+            <button>Ca</button>
+            </div>
+        </div>
+
+    </div>`);
+
+}
+
 $.modal = function(options){
     return
 
