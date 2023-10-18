6e17747ea31d95366d01dc7e70a2e0453767f4d3
Aelexx <140091101+Aelexx@users.noreply.github.com>
2023-10-18 12:01:59 +0300

    zxcmm

--- plugins/modal.js
+++ plugins/modal.js
@@ -4,12 +4,12 @@ function _createModal (options){
     modal.classList.add('amodal');
     modal.insertAdjacentHTML('afterbegin', `   
 
-    <div class="modal-overlay">
+    <div class="modal-overlay" data-close>
         <div class="modal-window" style="width: ${options.width || defwidth}">
 
             <div class="modal-header">
             <span class="modal-title">${options.title || ''}</span>    
-            ${options.closable ? `<span class="modal-close">&times;</span>` : ''}
+            ${options.closable ? `<span class="modal-close" data-close>&times;</span>` : ''}
             </div>
 
             <div class="modal-body">
@@ -31,6 +31,9 @@ function _createModal (options){
 $.modal = function(options){
     const $modal = _createModal(options)
     let closing = false;
+    $modal = addEventListener('click', event => {
+        console.log("Clicked!", event.target)
+    })
     return {
         open(){
          !closing && $modal.classList.add('open')
