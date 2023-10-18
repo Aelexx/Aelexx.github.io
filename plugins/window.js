8ff8b299228516e37b9693261719fab03665886a
Aelexx <140091101+Aelexx@users.noreply.github.com>
2023-10-18 16:15:09 +0300

    Node

--- plugins/window.js
+++ plugins/window.js
@@ -1,6 +1,6 @@
-function createWindow(options){
+function createElements(options){
     const newDiv = document.createElement('div')
-    const newContent = document.textContent('Mouse and rat Rato e Gato')
+    const newContent = document.textNode('Mouse and rat Rato e Gato')
     newDiv.appendChild(newContent)
 
     const currentH1 = document.getElementById('id-dyn')
