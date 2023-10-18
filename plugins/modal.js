550d97103f2ff651d794f5ec91f465cd0c3fe786
Aelexx <140091101+Aelexx@users.noreply.github.com>
2023-10-18 09:49:59 +0300

    ksbda

--- plugins/modal.js
+++ plugins/modal.js
@@ -29,26 +29,26 @@ function _createModal (){
 
 $.modal = function(options){
     const $modal = _createModal(options)
-    let closing = false;
+    //let closing = false;
     return {
         open(){
-         !closing && $modal.classList.add('open')
+         $modal.classList.add('open')
             
         },
         close(){
-            closing = true
+            //closing = true
             $modal.classList.remove('open')
             $modal.classList.add('hide')
                setTimeout( () => {
                   $modal.classList.remove('hide')
-                   closing = false
+                   //closing = false
                }, 20000)
             
         },
-        destroy(){
-            closing = true
-            $modal.classList.remove('open')
-            closing = false
-        }
+        // destroy(){
+        //     closing = true
+        //     $modal.classList.remove('open')
+        //     closing = false
+        // }
     }
 }
