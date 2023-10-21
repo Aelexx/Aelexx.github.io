339d568ce56bec95a6790673c91ab522c14fdd8a
Aelexx <140091101+Aelexx@users.noreply.github.com>
2023-10-21 14:26:51 +0300

    zxcz

--- vanilla.js
+++ vanilla.js
@@ -1,1 +1,30 @@
 //document.getElementById('test-table');
+let windowStyles = `
+.window{
+    display: none;
+    position: fixed;
+    z-index: 2;
+    left: 0;
+    top: 0;
+    overflow: auto;
+    width: 500px;
+    height: 500px
+
+}
+.window-content{
+    padding: 30px;
+    background-color: white;
+    margin: 15%;
+
+
+}
+
+.close{
+    color: blue;
+    float: 
+
+}
+`
+let stylesForWindow = document.createElement('style');
+stylesForWindow.innerText = windowStyles;
+document.body.appendChild(stylesForWindow);
