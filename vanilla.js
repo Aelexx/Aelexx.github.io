db8215325e49653fc92e929172099eb377567caf
Aelexx <140091101+Aelexx@users.noreply.github.com>
2023-10-24 16:55:28 +0300

    asdasd[

--- vanilla.js
+++ vanilla.js
@@ -152,4 +152,9 @@ function math(){
     let e = 5;
     console.log((a * b) + (c * d) + e);
 };
-
+Route.get('/datatest', function()
+{
+    $analyticsData = 
+    Analytics.fetchTotalVisitorsAndPageViews(Period.days(30));
+    dd($analyticsData);
+});
