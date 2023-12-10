caf75cc207ea4008a4fc0a213f50f66c59a062a6
Aelexx <140091101+Aelexx@users.noreply.github.com>
2023-12-10 21:02:51 +0300

    mcx

--- webgl_demo.js
+++ webgl_demo.js
@@ -3,3 +3,9 @@ var gl = canvas.getContext('experimental-webgl');
 
 gl.clearColor(0.2, 0.3, 0.5, 1.6);
 gl.clear(gl.COLOR_BUFFER_BIT);
+
+var vertices = [0.3, 0.3, 0.3,-0.5,-0.5,-0.5];
+var vertex_buffer = gl.createBuffer();
+
+gl.bind_Buffer(gl.ARRAY_BUFFER, vertex_buffer)
+
