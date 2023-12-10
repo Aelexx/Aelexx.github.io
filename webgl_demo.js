795e5a0fffa1409ef86600852a190e7fbb44a8f8
Aelexx <140091101+Aelexx@users.noreply.github.com>
2023-12-10 17:25:47 +0300

    sldkad

--- webgl_demo.js
+++ webgl_demo.js
@@ -1,5 +1,5 @@
-var canvas = document.getElementById("mycanvas");
-var gl = canvas.getContext("webgl_mine");
+var canvas = document.getElementById('mycanvas');
+var gl = canvas.getContext('experimental-webgl');
 
 gl.clearColor(0.2, 0.3, 0.5, 1.6);
 gl.clear(gl.COLOR_BUFFER_BIT);
