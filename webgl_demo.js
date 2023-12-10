961581064a47ffa6ff5615cbde21d2f5995028ac
Aelexx <140091101+Aelexx@users.noreply.github.com>
2023-12-10 16:59:51 +0300

    dfn

--- webgl_demo.js
+++ webgl_demo.js
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html>
-   <canvas id = 'my_canvas'></canvas>
-	
-   <script>
-      var canvas = document.getElementById('my_canvas');
-      var gl = canvas.getContext('experimental-webgl');
-      gl.clearColor(0.9,0.9,0.8,1);
-      gl.clear(gl.COLOR_BUFFER_BIT);
-   </script>
-</html>
+var canvas = document.getElementById(#mycanvas);
+var gl = canvas.getContext("webgl_mine");
+
+gl.clearColor(#f9f9f9);
