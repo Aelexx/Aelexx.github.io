06aafe663d2c621f29e17e7a879d37a8238f699d
Aelexx <140091101+Aelexx@users.noreply.github.com>
2023-12-11 20:30:57 +0300

    ddw

--- webgl_demo.js
+++ webgl_demo.js
@@ -7,5 +7,30 @@ gl.clear(gl.COLOR_BUFFER_BIT);
 var vertices = [0.3, 0.3, 0.3,-0.5,-0.5,-0.5];
 var vertex_buffer = gl.createBuffer();
 
-gl.bind_Buffer(gl.ARRAY_BUFFER, vertex_buffer)
+gl.bind_Buffer(gl.ARRAY_BUFFER, vertex_buffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+gl.bind_Buffer(gl.ARRAY_BUFFER, null);
+
+var vertCode = 
+    "attribute vec2 coordinates;" +
+    "void main(void) {"+" gl_Position = vec4(coordinates, 0.0, 1.0); "+"}"
+
+var vertShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource = (vertShader, vertCode);
+gl.compileShader(vertShader);
+
+var fragCode = "void main(void) {"+" gl_FragColor = vec4(0.0, 0.0, 0.0, 0.1); "+"}";
+var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource = (fragShader, fragCode);
+gl.compileShader(fragShader);
+
+var shaderProgram = gl.createProgram();
+gl.attachShader(shaderProgram, vertShader);
+gl.attachShader(shaderProgram, fragShader);
+
+gl.linkProgram(shaderProgram);
+
+
+
 
