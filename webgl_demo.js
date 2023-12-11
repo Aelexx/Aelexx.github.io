30c7366d525a60f2c488989391cdca1f916aae81
Aelexx <140091101+Aelexx@users.noreply.github.com>
2023-12-11 20:47:10 +0300

    sqsqe

--- webgl_demo.js
+++ webgl_demo.js
@@ -39,6 +39,15 @@ gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0);
 
 gl.enableVertexAttribArray(coord);
 
+gl.clearColor(0.5, 0.5, 0.5, 0.9);
+gl.enable(gl.DEPTH_TEST);
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+gl.viewport(0, 0, canvas.width, canvas.height);
+
+gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+
 
 
 
