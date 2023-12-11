a508313201df2d3af3f865618277eaf43b61fdd1
Aelexx <140091101+Aelexx@users.noreply.github.com>
2023-12-11 20:51:20 +0300

    fdf

--- webgl_demo.js
+++ webgl_demo.js
@@ -1,10 +1,7 @@
 var canvas = document.getElementById('mycanvas');
 var gl = canvas.getContext('experimental-webgl');
 
-gl.clearColor(0.2, 0.3, 0.5, 1.6);
-gl.clear(gl.COLOR_BUFFER_BIT);
-
-var vertices = [0.3, 0.3, 0.3,-0.5,-0.5,-0.5];
+var vertices = [-0.3, 0.3, -0.3, -0.5, 0.5, -0.5];
 var vertex_buffer = gl.createBuffer();
 
 gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
