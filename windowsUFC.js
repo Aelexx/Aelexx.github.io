0fef6d459a66805384591949386e2f26a637765f
Aelexx <140091101+Aelexx@users.noreply.github.com>
2024-01-27 16:38:07 +0300

    xvcvm

--- windowsUFC.js
+++ windowsUFC.js
@@ -1,1 +1,7 @@
-let fighterName = document.getElementById("stbtn")
+var form = document.getElementById('stbtn');
+form.style.display = 'none';
+
+var processing = document.createElement('span');
+processing.appendChild(document.createTextNode('processing ...'));
+
+form.parentNode.insertBefore(processing, form);
