03ae0aefe315f67ec259bd682783e1b73010b561
Aelexx <140091101+Aelexx@users.noreply.github.com>
2024-01-27 16:48:34 +0300

    xcvcvm

--- windowsUFC.js
+++ windowsUFC.js
@@ -1,7 +1,1 @@
-var form = document.getElementById('stbtn');
-form.style.display = 'none';
-
-var processing = document.createElement('span');
-processing.appendChild(document.createTextNode('processing ...'));
-
-form.parentNode.insertBefore(processing, form);
+let firstWindow = document.getElementById("stbtn").submit();
