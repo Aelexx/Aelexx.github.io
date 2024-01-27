7f144f01eae14814aa53ba9d1d50c575379f0ef8
Aelexx <140091101+Aelexx@users.noreply.github.com>
2024-01-27 21:26:03 +0300

    xjcvn

--- windowsUFC.js
+++ windowsUFC.js
@@ -7,4 +7,24 @@ document.getElementById("myDropdown").classList.toggle("show");
 function mFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
-  }
+}
+
+/* The `myFun()` function is prompting the user to enter their name using the `prompt()` function. It
+then checks if the user entered a value or cancelled the prompt. If the user entered a value, it
+assigns the value to the `fighter` variable and creates a string using the value. If the user
+cancelled the prompt or did not enter a value, it assigns a different string to the `text` variable.
+Finally, it sets the innerHTML of an element with the id "demo" to the value of the `text` variable. */
+function myFun() {
+    /* The line `let text;` is declaring a variable named `text` using the `let` keyword. This variable
+    is initially undefined and will be used to store a string value later in the code. */
+    let text;
+    /* The variable `fighter` is used to store the value entered by the user in response to the
+    prompt. */
+    let fighter = prompt("Please enter your name:", "Harry Potter");
+    if (fighter == null || fighter == "") {
+    text = "You cancelled the count run";
+    } else {
+    text = "Fighter: " + fighter + "! How are you today?";
+    }
+    document.getElementById("demo").innerHTML = text;
+}
