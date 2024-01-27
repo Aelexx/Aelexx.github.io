fe73dd10b3fb07653665b8553c2b28b00f6a62a5
Aelexx <140091101+Aelexx@users.noreply.github.com>
2024-01-27 16:36:18 +0300

    xcvx

--- windowsUFC.js
+++ windowsUFC.js
@@ -1,6 +1,1 @@
-let fighterName = prompt("Enter fighter's name", "Waiting...");
-
-if (person != null) {
-document.getElementById("demo").innerHTML =
-"Hello " + person + "! How are you today?";
-}
+let fighterName = document.getElementById("stbtn")
