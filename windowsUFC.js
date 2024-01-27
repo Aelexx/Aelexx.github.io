700c4cdd5c685e40d984d8c88fe8a9fe6dec456f
Aelexx <140091101+Aelexx@users.noreply.github.com>
2024-01-27 20:36:18 +0300

    cvb

--- windowsUFC.js
+++ windowsUFC.js
@@ -3,3 +3,8 @@ document.getElementById("myBtn").onclick = function() {myFunction()};
 function myFunction() {
 document.getElementById("myDropdown").classList.toggle("show");
 }
+
+function mFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
