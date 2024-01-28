d2f9afd2eda00b4a9c5f3f9c01967133a8d503d1
Aelexx <140091101+Aelexx@users.noreply.github.com>
2024-01-28 11:31:02 +0300

    xcvjn

--- windowsUFC.js
+++ windowsUFC.js
@@ -4,13 +4,10 @@ assigns the value to the `fighter` variable and creates a string using the value
 cancelled the prompt or did not enter a value, it assigns a different string to the `text` variable.
 Finally, it sets the innerHTML of an element with the id "demo" to the value of the `text` variable. */
 function myFun() {
-    /* The line `let text;` is declaring a variable named `text` using the `let` keyword. This variable
-    is initially undefined and will be used to store a string value later in the code. */
+
     let text;
-    /* The variable `fighter` is used to store the value entered by the user in response to the
-    prompt. */
-    let fighter = prompt("Input name of fighter:", "Name of fighter");
-    if (fighter == null || fighter == "") {
+    let fighter = prompt("Input name of fighter:", "...");
+    if (fighter == null || fighter == "" || fighter == '0-9') {
     text = "You cancelled the count run";
     } else {
     text = "Fighter: " + fighter + " Got in the base";
