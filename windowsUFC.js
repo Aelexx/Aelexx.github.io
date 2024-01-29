7a701b17422f19d2abe2ae10e23e2773e481923c
Aelexx <140091101+Aelexx@users.noreply.github.com>
2024-01-29 17:12:45 +0300

    xjnvc

--- windowsUFC.js
+++ windowsUFC.js
@@ -3,7 +3,7 @@ then checks if the user entered a value or cancelled the prompt. If the user ent
 assigns the value to the `fighter` variable and creates a string using the value. If the user
 cancelled the prompt or did not enter a value, it assigns a different string to the `text` variable.
 Finally, it sets the innerHTML of an element with the id "demo" to the value of the `text` variable. */
-function myFun() {
+function myFunName() {
 
     let text;
     let fighter = prompt("Input name of fighter:", "");
@@ -15,7 +15,7 @@ function myFun() {
         alert("Only letters we need");
         validate()
     } 
-    
+
     
     else {
     text = "Fighter: " + fighter + " now in the base";
