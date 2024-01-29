c5cd2146530e0aeb4b49799dcda640f09251b9ac
Aelexx <140091101+Aelexx@users.noreply.github.com>
2024-01-29 15:26:14 +0300

    ksjdfn

--- windowsUFC.js
+++ windowsUFC.js
@@ -40,3 +40,35 @@ function myFunWin() {
     }
     document.getElementById("messFightersName").innerHTML = text;
 }
+
+//pop1 
+var e = document.getElementById('popupBoxOnePosition');
+ var close = document.getElementById('closebox1');
+
+document.getElementById('box1').addEventListener('click', function(){
+  if(e.style.display == 'block')
+    e.style.display = 'none';
+  else
+    e.style.display = 'block';
+});
+
+// When the user clicks on Click here to close modal
+close.onclick = function() {
+    e.style.display = "none";
+}
+
+//pop2 js
+ var elpopupBoxTwoPosition = document.getElementById('popupBoxTwoPosition');
+ var closeclosebox2 = document.getElementById('closebox2');
+
+document.getElementById('box2').addEventListener('click', function(){
+  if(elpopupBoxTwoPosition.style.display == 'block')
+    elpopupBoxTwoPosition.style.display = 'none';
+  else
+    elpopupBoxTwoPosition.style.display = 'block';
+});
+  
+// When the user clicks on Click here to close modal
+closeclosebox2.onclick = function() {
+    elpopupBoxTwoPosition.style.display = "none";
+}
