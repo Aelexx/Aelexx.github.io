629d86b2def0b21148726871a97552f6c1d203d2
Aelexx <140091101+Aelexx@users.noreply.github.com>
2024-01-31 22:32:35 +0300

    jcvnb

--- windowsUFC.js
+++ windowsUFC.js
@@ -14,12 +14,12 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function(event) {
+btn.onclick = function() {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function(event) {
+span.onclick = function() {
   modal.style.display = "none";
 }
 
