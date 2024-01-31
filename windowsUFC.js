9ac0be3665a436d73333b368d28697a1e84aab4c
Aelexx <140091101+Aelexx@users.noreply.github.com>
2024-01-31 22:58:31 +0300

    xkjcv

--- windowsUFC.js
+++ windowsUFC.js
@@ -5,30 +5,35 @@ assigns the value to the `fighter` variable and creates a string using the value
 cancelled the prompt or did not enter a value, it assigns a different string to the `text` variable.
 Finally, it sets the innerHTML of an element with the id "demo" to the value of the `text` variable. */
 
-var modal = document.getElementById("myModal");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// close modal function
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// close the modal when the close button and overlay is clicked
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+// close modal when the Esc key is pressed
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
   }
-}
+});
+
+// open modal function
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+// open modal event
+openModalBtn.addEventListener("click", openModal);
 
 function myFunName() {
 
