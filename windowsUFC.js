c303e09f9a51de065283948033ebd2080430b796
Aelexx <140091101+Aelexx@users.noreply.github.com>
2024-02-03 17:17:57 +0300

    jxncv

--- windowsUFC.js
+++ windowsUFC.js
@@ -1,9 +1,10 @@
-// const { text } = require("stream/consumers");
-/* The `myFun()` function is prompting the user to enter their name using the `prompt()` function. It
-then checks if the user entered a value or cancelled the prompt. If the user entered a value, it
-assigns the value to the `fighter` variable and creates a string using the value. If the user
-cancelled the prompt or did not enter a value, it assigns a different string to the `text` variable.
-Finally, it sets the innerHTML of an element with the id "demo" to the value of the `text` variable. */
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
 
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
