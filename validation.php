f3583ce950625f09b83c536ea3f2a7a5b21dc098
Aelexx <140091101+Aelexx@users.noreply.github.com>
2024-02-03 16:36:29 +0300

    xmcvl

--- validation.php
+++ validation.php
@@ -1,27 +1,38 @@
 <?php
-// define variables and set to empty values
-$nameErr = "";
-$name = "";
+$value = $_GET['query'];
+$formfield = $_GET['field'];
+// Check Valid or Invalid user name when user enters user name in username input field.
+if ($formfield == "username") {
+if (strlen($value) < 4) {
+echo "Must be 3+ letters";
+} else {
+echo "<span>Valid</span>";
+}
+}
+// Check Valid or Invalid password when user enters password in password input field.
+if ($formfield == "password") {
+if (strlen($value) < 6) {
+echo "Password too short";
+} else {
+echo "<span>Strong</span>";
+}
+}
+// Check Valid or Invalid email when user enters email in email input field.
+if ($formfield == "email") {
+if (!preg_match("/([w-]+@[w-]+.[w-]+)/", $value)) {
+echo "Invalid email";
+} else {
+echo "<span>Valid</span>";
+}
+}
+// Check Valid or Invalid website address when user enters website address in website input field.
+if ($formfield == "website") {
+if (!preg_match("/b(?:(?:https?|ftp)://|www.)[-a-z0-9+&@#/%?=~_|!:,.;]*[-a-z0-9+&@#/%=~_|]/i", $value)) {
+echo "Invalid website";
+} else {
+echo "<span>Valid</span>";
+}
+}
+?>
+Copy
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") 
-{
-    if (empty($_POST["name"])) 
-    {
-    $nameErr = "Name is required";
-    } 
-    else 
-    {
-    $name = test_input($_POST["name"]);
-      // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) 
-    {
-        $nameErr = "Only letters and white space allowed";
-    }
-}
-}
-function test_input($data) {
-$data = trim($data);
-$data = stripslashes($data);
-$data = htmlspecialchars($data);
-return $data;
-}
