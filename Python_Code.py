73b6da9ae280ff66a592f5b3d14c54004e9d0e0b
Aelexx <140091101+Aelexx@users.noreply.github.com>
2024-04-10 20:44:16 +0300

    xcvhcv

--- Python_Code.py
+++ Python_Code.py
@@ -1,9 +1,9 @@
-# print("Hi python!")
-# print(2+6)
-# fahrenheit = int(input("Fahrenheit temprature: "))
+print("Hi python!")
+print(2+6)
+fahrenheit = int(input("Fahrenheit temprature: "))
 
-# celsius = (fahrenheit - 32) * (5/9)
-# celsius_round = round(celsius)
-# print(str(fahrenheit) + " degrees Fahrenheit = " + str(celsius_round) + "degree Celsius")
+celsius = (fahrenheit - 32) * (5/9)
+celsius_round = round(celsius)
+print(str(fahrenheit) + " degrees Fahrenheit = " + str(celsius_round) + "degree Celsius")
 import sys
 print('Helo, my name is ', sys.argv[1])
