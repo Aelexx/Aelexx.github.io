b7bb4a5297d821bdcd1b571fe3dac2e417cdcd77
Aelexx <140091101+Aelexx@users.noreply.github.com>
2024-04-10 17:20:14 +0300

    xcvm

--- xhr.js
+++ xhr.js
@@ -3,13 +3,14 @@
 function loadDoc() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-       document.getElementById("demo").innerHTML = this.responseText;
-      }
+    if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("demo").innerHTML = this.responseText;
+    }
     };
     xhttp.open("GET", "LoadTxt.txt", true);
     xhttp.send();
-  }
+}
+
 const captBrasil = document.querySelector('.caption_Brasil');
 const offsParent = captBrasil.offsetParent;
 console.log(offsParent);
