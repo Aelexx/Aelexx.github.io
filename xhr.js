7077ee6edc1e6ee98b3bdc3892a2c4bbfcfcb72a
Aelexx <140091101+Aelexx@users.noreply.github.com>
2024-04-10 17:15:54 +0300

    xjcn

--- xhr.js
+++ xhr.js
@@ -1,5 +1,15 @@
 'use restrict'
 
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("demo").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "ajax_info.txt", true);
+    xhttp.send();
+  }
 const captBrasil = document.querySelector('.caption_Brasil');
 const offsParent = captBrasil.offsetParent;
 console.log(offsParent);
