2da5250dfb1faa2ecc4591818000601f2dba7c67
Aelexx <140091101+Aelexx@users.noreply.github.com>
2024-04-11 18:12:05 +0300

    Delete Python_Code.py

--- Python_Code.py
+++
@@ -1,9 +0,0 @@
-print("Hi python!")
-print(2+6)
-fahrenheit = int(input("Fahrenheit temprature: "))
-
-celsius = (fahrenheit - 32) * (5/9)
-celsius_round = round(celsius)
-print(str(fahrenheit) + " degrees Fahrenheit = " + str(celsius_round) + "degree Celsius")
-import sys
-print('Helo, my name is ', sys.argv[1])
