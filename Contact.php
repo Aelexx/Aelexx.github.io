7a824987a46558d42bbc1ce39b20e382b9faa402
Aelexx <140091101+Aelexx@users.noreply.github.com>
2024-04-13 20:31:10 +0300

    xcvn

---
+++ Contact.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> Contact form </title>
+    </head>
+    <body>
+        <main>
+            <h6> Send mail </h6>
+            
+
+        </main>
+
+    </body>
+
+</html>
