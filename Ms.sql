cf9a098eb0a67402a0628284ed523662209ba93b
Alex <140091101+Aelexx@users.noreply.github.com>
2024-07-11 11:29:26 +0300

    Delete Ms.sql

--- Ms.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE BRASIL (
-    empId INTEGER PRIMARY KEY,
-    provincia TEXT NOT NULL,
-    city TEXT NOT NULL,
-    district TEXT NOT NULL
-);
-INSERT INTO BRASIL VALUES (001, 'Bahia', 'Rio', 'Capareila');
